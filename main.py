bf858cd204f440db458fa1c469438b637b28be29
Arthur César <arthur.cesar@aluno.impacta.edu.br>
2026-04-08 21:44:44 -0300

    bug proposital

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
 @app.get("/")
 def root():
     return {"msg": "API rodando"}
 
 @app.get("/somar/{a}/{b}")
 def somar(a: int, b: int):
-    return {"resultado": a + b}
+    return {"resultado": a - b}
