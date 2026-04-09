0b295af60eaf1b808e36a685278dbbd42e6747d8
Arthur César <arthur.cesar@aluno.impacta.edu.br>
2026-04-08 22:03:20 -0300

    feat: multiplicacao

--- test_main.py
+++ test_main.py
@@ -7,3 +7,6 @@ def test_somar():
     response = client.get("/somar/10/20")
     assert response.json() == {"resultado": 30}
 
+def test_multiplicar():
+    response = client.get("/multiplicar/2/2")
+    assert response.json() == {"resultado": 5}
